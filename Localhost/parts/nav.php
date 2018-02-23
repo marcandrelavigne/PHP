efc65ba260548b744042c42eb56cf102666312f5
marcandrelavigne <marcandre.lavigne@me.com>
2018-02-23 14:25:18 -0500

    Update nav.php

--- Localhost/parts/nav.php
+++ Localhost/parts/nav.php
@@ -1,11 +1,8 @@
 <div class="project">
-
     <nav class="project-nav">
-        <a href="http://localhost/bravad/" class="project-link bravadtab">Bravad</a>
-        <a href="http://localhost/playground/" class="project-link">Playground</a>
-        <a href="http://localhost/plugins/" class="project-link">PHP Plugins</a>
-        <a href="http://localhost/vueJS/" class="project-link">Vue.js</a>
-        <a href="http://localhost/wordpress-sandbox/" class="project-link">WP Sandbox</a>
+        <a href="http://localhost/project1/" class="project-link">Project 1</a>
+        <a href="http://localhost/project2/" class="project-link">Project 2</a>
+        <a href="http://localhost/project3/" class="project-link">Project 3</a>
         <a href="http://localhost/phpMyAdmin/" class="link-ext"><em>php</em>MyAdmin</a>
     </nav>
 </div>
