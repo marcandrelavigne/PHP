acb1ba764afde6d1ecb2f926de506bd0d8497114
marcandrelavigne <marcandre.lavigne@me.com>
2018-02-23 14:21:38 -0500

    Add files via upload

---
+++ Localhost/index.php
@@ -0,0 +1,19 @@
+<?php include( 'parts/header.php' ); ?>
+
+    <div class="wrapper">
+
+        <?php
+			// Include Nav
+			include( 'parts/nav.php' );
+		?>
+
+        <div class="wrapper-inner">
+
+            <?php include( 'parts/content-home.php' ); ?>
+
+            <?php include( 'parts/sidebar.php' ); ?>
+
+        </div>
+    </div>
+
+<?php include( 'parts/footer.php' ); ?>
